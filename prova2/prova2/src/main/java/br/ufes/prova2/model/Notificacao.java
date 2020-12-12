162d6859a2545b2923ebd51cc93a1ff5610067af
= <=>
2020-12-12 14:29:52 -0300

    projeto inicial

---
+++ prova2/prova2/src/main/java/br/ufes/prova2/model/Notificacao.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufes.prova2.model;
+
+/**
+ *
+ * @author 55289
+ */
+public class Notificacao {
+    public int id;
+    public int idUsuario;
+    public String descricao;
+
+    public Notificacao(int id, int idUsuario) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    
+}
